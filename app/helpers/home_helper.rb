8b189817c97d7f501a440e178a98fba31a304657
Jesse Kershaw <jesse.kershaw@gmail.com>
2013-09-08 22:47:52 +1200

    Replace bootstrap with foundation css framework

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+  def user_resource_name
+    :user
+  end
+
+  def user_resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
