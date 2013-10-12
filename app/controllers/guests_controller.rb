577752614e7fdd138d2245a72923de95640c05d6
Jesse Kershaw <jesse.kershaw@gmail.com>
2013-10-12 15:36:05 +1300

    Fix attending_state_id permit

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -82,6 +82,6 @@ class GuestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def guest_params
-      params.require(:guest).permit(:user_id, :attending_state)
+      params.require(:guest).permit(:user_id, :attending_state_id)
     end
 end
