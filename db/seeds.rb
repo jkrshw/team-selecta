d1b2f4a7e522d9cf9ecb10409497ffb16f40cb8f
Jesse Kershaw <jesse.kershaw@gmail.com>
2013-10-12 23:45:51 +1300

    Add seeds with example data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# application data
+attending_states = AttendingState.create([
+  {name: 'invited'},
+  {name: 'attending'},
+  {name: 'maybe'},
+  {name: 'not_attending'}
+  ])
+
+# example data
 clubs = Club.create([
 	{ name: 'Hockey Club', description: 'Varsity Hockey' },
   { name: 'Football Club', description: 'Varsity Hockey'  },
@@ -17,9 +19,41 @@ clubs = Club.create([
   { name: 'Tramping Club', description: 'Varsity Hockey'  }
   ])
 
-attending_states = AttendingState.create([
-  {name: 'invited'},
-  {name: 'attending'},
-  {name: 'maybe'},
-  {name: 'not_attending'}
+events = Event.create([
+  {name: 'Winter Trials - Prems', description: '2014 Hockey trials', location: 'AGS Turf', club: clubs[0], time:DateTime.strptime("11/11/2013 15:00", "%m/%d/%Y %H:%M")},
+  {name: 'Winter Trials - Champ', description: '2014 Hockey trials', location: 'DIO Turf', club: clubs[0], time:DateTime.strptime("11/11/2013 15:00", "%m/%d/%Y %H:%M")},
+  {name: 'Trials - Indoor', description: '2014 Hockey trials', location: 'Auckland Domain', club: clubs[1], time:DateTime.strptime("11/11/2013 15:00", "%m/%d/%Y %H:%M")},
+  {name: 'Uni Games', description: '2014 Hockey trials', location: 'Uni Gym', club: clubs[2], time:DateTime.strptime("11/11/2013 15:00", "%m/%d/%Y %H:%M")},
+  {name: 'Social', description: '2014 Hockey trials', location: 'Epsom', club: clubs[2], time:DateTime.strptime("11/11/2013 15:00", "%m/%d/%Y %H:%M")}
   ])
+
+users = User.create([
+  {name: 'Joe Blogs', email:'test1@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Jane Ace', email:'test2@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Joe Black', email:'test3@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Sheila Allen', email:'test4@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Anna Strasberg', email:'test5@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Mimi Rogers', email:'test6@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Bo Derek', email:'test7@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Loretta Lynn', email:'test8@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Gracie Lantz', email:'test9@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Dana Reeve', email:'test10@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Stella Stevens', email:'test11@example.com', password: Devise.friendly_token[0,20]},
+  {name: 'Demi Moore', email:'test12@example.com', password: Devise.friendly_token[0,20]}
+  ])
+
+events.each do |event|
+  event.guests.create([
+  {event: event, user_id: users[0].id, attending_state: attending_states[0]},
+  {event: event, user_id: users[1].id, attending_state: attending_states[0]},
+  {event: event, user_id: users[2].id, attending_state: attending_states[1]},
+  {event: event, user_id: users[4].id, attending_state: attending_states[1]},
+  {event: event, user_id: users[5].id, attending_state: attending_states[1]},
+  {event: event, user_id: users[6].id, attending_state: attending_states[1]},
+  {event: event, user_id: users[7].id, attending_state: attending_states[2]},
+  {event: event, user_id: users[8].id, attending_state: attending_states[2]},
+  {event: event, user_id: users[9].id, attending_state: attending_states[2]},
+  {event: event, user_id: users[10].id, attending_state: attending_states[3]},
+  {event: event, user_id: users[11].id, attending_state: attending_states[3]}
+  ])
+end
