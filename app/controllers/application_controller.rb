79d3f7d2a8200b29c02d92bb50fb193b70003427
Jesse Kershaw <jesse.kershaw@gmail.com>
2013-10-17 22:17:50 +1300

    Allow editing of user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
-	#todo, this is the lazy way to allow signup params
-	before_filter :configure_permitted_parameters, if: :devise_controller?
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   protected
 
-  	def configure_permitted_parameters
-  		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:password,:password_confirmation, :username, :email, :name) }
-		end
+	def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super
+    end
+  end
 end
